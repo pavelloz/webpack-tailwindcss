2ca630954de27ca1e340715a7ac5d828d41799eb
Pawel Kowalski <pavelloz@gmail.com>
2022-03-20 23:28:00 +0100

    Update npm dependencies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-const brandColors = {
-  green: '#2b322b',
-};
-
-const systemColors = {
-  blue: '#007aff',
-};
-
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-      colors: {
-        ...brandColors,
-        ...systemColors,
-      },
       fontFamily: {
         sans: ['Lato', ...defaultTheme.fontFamily.sans],
       },
@@ -27,5 +15,8 @@ module.exports = {
   variants: {
     borderWidth: ['responsive', 'hover'],
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms', {
+    strategy: 'base', // only generate global styles
+    // strategy: 'class', // only generate classes
+  })],
 };
